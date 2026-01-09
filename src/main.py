306a7191a0168b9dd73c99b273f4b1a5cde3af76
Michael Horstmann <mhorstmann@gams.com>
2026-01-09 15:02:28 +0100

    Improve example code

--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python3
-
 import sys
 
 
 def main() -> int:
-    print('Hello world!')
+    print("Hello world!")
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
